523a74109a8047b4086559ce4beb1fbc1038561a
Yuheng Su <gipsyh.icu@gmail.com>
2025-12-09 16:00:21 +0800

    relax update condition of nerv reg check

--- riscv-formal/nerv/reg/nerv.sv
+++ riscv-formal/nerv/reg/nerv.sv
@@ -1224,12 +1224,11 @@ module nerv #(
 					assert(register_shadow == rvfi_rs1_rdata[`RISCV_FORMAL_CHANNEL_IDX*`RISCV_FORMAL_XLEN +: `RISCV_FORMAL_XLEN]);
 				if (register_written && register_index == rvfi_rs2_addr[`RISCV_FORMAL_CHANNEL_IDX*5 +: 5])
 					assert(register_shadow == rvfi_rs2_rdata[`RISCV_FORMAL_CHANNEL_IDX*`RISCV_FORMAL_XLEN +: `RISCV_FORMAL_XLEN]);
-			end else begin
-				for (channel_idx = 0; channel_idx < `RISCV_FORMAL_NRET; channel_idx=channel_idx+1) begin
-					if (rvfi_valid[channel_idx] && !rvfi_trap[channel_idx] && rvfi_order[64*channel_idx +: 64] < insn_order && register_index == rvfi_rd_addr[channel_idx*5 +: 5]) begin
-						register_shadow = rvfi_rd_wdata[channel_idx*`RISCV_FORMAL_XLEN +: `RISCV_FORMAL_XLEN];
-						register_written = 1;
-					end
+			end
+			for (channel_idx = 0; channel_idx < `RISCV_FORMAL_NRET; channel_idx=channel_idx+1) begin
+				if (rvfi_valid[channel_idx] && !rvfi_trap[channel_idx] && register_index == rvfi_rd_addr[channel_idx*5 +: 5]) begin
+					register_shadow = rvfi_rd_wdata[channel_idx*`RISCV_FORMAL_XLEN +: `RISCV_FORMAL_XLEN];
+					register_written = 1;
 				end
 			end
 		end
