2109f8c487facc218f3365f76fdaa0372c452893
Yuheng Su <gipsyh.icu@gmail.com>
2026-05-26 12:25:51 +0000

    update

--- frame_proc/invariants.sv
+++ frame_proc/invariants.sv
@@ -2,8 +2,7 @@ module invariants (
     input logic clk,
     input logic rst_n
 );
-    P0 :
-    assert property (@(posedge clk) disable iff (!rst_n) frame_proc.u_main_fifo.count != 5);
+// To use a signal from frame_proc, reference it as frame_proc.XXX.
 endmodule
 
 bind frame_proc invariants invariants (.*);
